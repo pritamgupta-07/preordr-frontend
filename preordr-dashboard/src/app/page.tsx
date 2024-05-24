cbf14fd1b1b0bce1e47e84dede566be7c2b03c20
pritam-guptaa <uniquepritam29@gmail.com>
2024-05-24 19:55:22 +0530

    test main branch protection

--- preordr-dashboard/src/app/page.tsx
+++ preordr-dashboard/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="app">
-      Sidebar here
+      Sidebar component here!
       <main className="content">
         <h1>Content here</h1>
         <p>eg:- dashboard or orders route content</p>
