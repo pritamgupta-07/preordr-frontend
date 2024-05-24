61547d145cd64d06c5bd83ecc05f38ee1a8acd14
pritam-guptaa <uniquepritam29@gmail.com>
2024-05-24 19:51:34 +0530

    test branch protection

--- preordr-dashboard/src/app/page.tsx
+++ preordr-dashboard/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div className="app">
-      Sidebar component here!
+      Sidebar here
       <main className="content">
         <h1>Content here</h1>
         <Box sx={{ color: colors.orangeAccent[500],  }}>hello</Box>
