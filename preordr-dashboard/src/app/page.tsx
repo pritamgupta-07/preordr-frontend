5a4598d632e40b05d9f511d1d4a23b4986cb36e4
pritam-guptaa <uniquepritam29@gmail.com>
2024-05-24 19:03:24 +0530

    initial commit

---
+++ preordr-dashboard/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="app">
+      Sidebar here
+      <main className="content">
+        <h1>Content here</h1>
+        <p>eg:- dashboard or orders route content</p>
+      </main>
+    </div>
+  );
+}
