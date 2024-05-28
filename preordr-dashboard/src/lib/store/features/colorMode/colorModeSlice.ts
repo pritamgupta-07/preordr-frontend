a15320338d30a86dfc4fbb7309586a0b25fe5213
pritam-guptaa <uniquepritam29@gmail.com>
2024-05-28 13:16:10 +0530

    added theme mode

---
+++ preordr-dashboard/src/lib/store/features/colorMode/colorModeSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type Mode = 'light' | 'dark';
+
+export interface ModeState {
+  mode: Mode;
+}
+
+const initialState: ModeState = {
+    mode: 'dark',
+  };
+  
+  const modeSlice = createSlice({
+    name: 'colorMode',
+    initialState,
+    reducers: {
+      toggleMode(state) {
+        state.mode = state.mode === 'light' ? 'dark' : 'light';
+      },
+    },
+  });
+  
+  export const { toggleMode } = modeSlice.actions;
+  export default modeSlice.reducer;
