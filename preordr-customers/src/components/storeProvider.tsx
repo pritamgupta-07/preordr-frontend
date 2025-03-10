32b3c4b60c7e90fc6ab9abd81823dcf42ab58bf8
0-Dinesh-Chandra-Pandey <dchandrap973@gmail.com>
2025-03-10 10:24:13 +0530

    added: store provider

--- preordr-customers/src/components/storeProvider.tsx
+++ preordr-customers/src/components/storeProvider.tsx
@@ -9,7 +9,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
